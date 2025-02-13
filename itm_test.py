330c8b7a6e30dc83c8a63c20baea64c834368c83
Igor-itm <thzakazpy@mail.ru>
2025-02-13 13:58:30 +0300

    Test task completed

---
+++ itm_test.py
@@ -0,0 +1,57 @@
+# module: itm_test.py
+
+import re
+
+
+def calculate(operator: str, a: int, b: int) -> int:
+    actions = {
+        '+': a + b,
+        '-': a - b,
+        '/': a // b,
+        '*': a * b,
+    }
+    return actions[operator]
+
+
+def get_number(num_str: str, lo=1, hi=10):
+    try:
+        num = int(num_str)
+    except ValueError:
+        raise Exception(f"Ошибка при вводе целого числа. Введено: '{num_str}'")
+
+    if not (lo <= num <= hi):
+       raise Exception(f"Число вне диапазона от {lo} до {hi} включительно. Введено: '{num_str}'")
+    return num
+
+
+def split_expression(expression: str):
+    # Регулярное выражение для разделения строки на 3 части: a, operator, b
+    pattern = r'^( *[-+]*[^-+/*]*)([-+/*]?)(.*)$'
+    match = re.match(pattern, expression)
+
+    if match:
+        a = match.group(1) if match.group(1) else ''
+        operator = match.group(2) if match.group(2) else ''
+        b = match.group(3) if match.group(3) else ''
+        return a, operator, b
+    else:
+        return None, None, None
+
+
+def main(expression: str) -> str:
+    a, operator, b = split_expression(expression)
+    if operator:
+        if not b.strip().isdigit():
+            raise Exception(f"Ошибка при вводе целого числа. Введено: '{operator}{b}'")
+
+        a, b = map(get_number, [a, b])
+        return str(calculate(operator, a, b))
+    else:
+        raise Exception("Строка не является математической операцией.")
+
+
+if __name__ == '__main__':
+    input_str: str = input("Введите выражение: ")
+    result = main(input_str)
+
+    print(f"Результат: {result}")
